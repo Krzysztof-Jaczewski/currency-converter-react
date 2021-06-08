6762244868736c5289e737e15cb783c915f7599b
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-06-08 16:48:18 +0200

    added Array witch currency objects

---
+++ src/Currency/index.js
@@ -0,0 +1,36 @@
+export const Currency = [
+    {
+        id:1,
+        name: "PLN",
+        fullName: "złotych",
+        value: 1,
+    },
+
+    {
+        id:2,
+        name: "CHF",
+        fullName: "Franków Szwajcarskich",
+        value: 4.0901,
+    },
+
+    {
+        id:3,
+        name: "USD",
+        fullName: "Dolarów Amerykańskich",
+        value: 3.6690,
+    },
+
+    {
+        id:4,
+        name: "GBP",
+        fullName: "Funtów brytyjskich",
+        value: 5.1889,
+    },
+
+    {
+        id:5,
+        name: "EUR",
+        fullName: "Euro",
+        value: 4.4674,
+    },
+]
