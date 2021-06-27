7dd13394c5b58184d50353e240f81bceb98099db
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-06-27 09:47:42 +0200

    clock state moved to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useState} from "react";
+import { useState, useEffect } from "react";
 import { Main } from "./Component/Main";
 import { Logo } from "./Component/Logo";
 import { Form } from "./Component/Form";
@@ -17,65 +17,77 @@ function App() {
 
   const [result, setResult] = useState(() => (0));
   const [resultUpdate, setResultUpdate] = useState(() => (0));
+  const [date, setDate] = useState(new Date());
+
 
   const [amount, setAmont] = useState(() => (0));
   const [selectedCurrencyId, setSelectedCurrencyId] = useState(() => (0));
   const [targetCurrencyId, setTargetCurrencyId] = useState(() => (1));
 
-const calculateResult = (amount, selectedCurrencyId, targetCurrencyId) => {
-  amount = +amount;
-  let result = selectedCurrencyId * amount / targetCurrencyId;
-  result = result.toFixed(2);
-  setResult(result);
-}
+  const calculateResult = (amount, selectedCurrencyId, targetCurrencyId) => {
+    amount = +amount;
+    let result = selectedCurrencyId * amount / targetCurrencyId;
+    result = result.toFixed(2);
+    setResult(result);
+  }
 
-return (
-  <Main>
-    <Logo />
-    <Clock/>
-    <Form
-      amount={amount}
-      selectedCurrencyId={selectedCurrencyId}
-      targetCurrencyId={targetCurrencyId}
-      setResultUpdate={setResultUpdate}
-      calculateResult={calculateResult}
-      result={result}
-    >
-      <Fieldset
-        title="Kwota do przeliczenia"
-        extraContent={
-          <ImputNumber
-            setAmont={setAmont}
-          />}
-        body={
-          <Select
-            currencies={currencies}
-            setSelectedCurrencyId={setSelectedCurrencyId}
-          />}
+  useEffect(() => {
+    let intervalID = setInterval(() =>
+      setDate(new Date())
+      , 1000);
+
+    return () => clearInterval(intervalID);
+  }, []);
+
+  return (
+    <Main>
+      <Logo />
+      <Clock
+      date={date}
       />
-      <Fieldset
-        title="Na jaką walutę"
-        body={
-          <RadioCheck
-            currencies={currencies}
-            setTargetCurrencyId={setTargetCurrencyId}
-            selectedCurrencyId={selectedCurrencyId}
-            targetCurrencyId={targetCurrencyId}
-          />}
-      />
-      <Fieldset
-        title="Wynik"
-        extraContent={
-          <Result
-            resultUpdate={resultUpdate}
-          />}
-      />
-      <Button
-        title="przelicz"
-      />
-    </Form>
-  </Main>
-);
+      <Form
+        amount={amount}
+        selectedCurrencyId={selectedCurrencyId}
+        targetCurrencyId={targetCurrencyId}
+        setResultUpdate={setResultUpdate}
+        calculateResult={calculateResult}
+        result={result}
+      >
+        <Fieldset
+          title="Kwota do przeliczenia"
+          extraContent={
+            <ImputNumber
+              setAmont={setAmont}
+            />}
+          body={
+            <Select
+              currencies={currencies}
+              setSelectedCurrencyId={setSelectedCurrencyId}
+            />}
+        />
+        <Fieldset
+          title="Na jaką walutę"
+          body={
+            <RadioCheck
+              currencies={currencies}
+              setTargetCurrencyId={setTargetCurrencyId}
+              selectedCurrencyId={selectedCurrencyId}
+              targetCurrencyId={targetCurrencyId}
+            />}
+        />
+        <Fieldset
+          title="Wynik"
+          extraContent={
+            <Result
+              resultUpdate={resultUpdate}
+            />}
+        />
+        <Button
+          title="przelicz"
+        />
+      </Form>
+    </Main>
+  );
 }
 
 export default App;
--- src/Component/Clock/index.js
+++ src/Component/Clock/index.js
@@ -1,17 +1,6 @@
 import "./style.css";
-import { useState, useEffect } from "react";
 
-export const Clock = () => {
-    const [date, setDate] = useState(new Date());
-
-    useEffect(() => {
-        let intervalID = setInterval(() =>
-            setDate(new Date())
-            , 1000);
-
-        return () => clearInterval(intervalID);
-    }, []);
-
+export const Clock = ({date}) => {
     const currentDate =
         date.toLocaleDateString(undefined,
             {
