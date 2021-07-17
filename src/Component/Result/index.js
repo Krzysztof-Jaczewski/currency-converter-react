17ccb7e57164cf00683ee758891444043d2de5da
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-07-17 18:41:59 +0200

    styled result and select complete

--- src/Component/Result/index.js
+++ src/Component/Result/index.js
@@ -1,9 +1,7 @@
-import "./style.css";
+import styled from "styled-components";
 
-export const Result = ({ resultUpdate }) => (
-    resultUpdate !== 0 && (
-        <label className="result">
-            {resultUpdate}
-        </label>
-    )
-);
+const StyledResult = styled.label`
+  margin: 5px auto;
+`;
+export const Result = ({ resultUpdate }) =>
+  resultUpdate !== 0 && <StyledResult>{resultUpdate}</StyledResult>;
--- src/Component/Select/index.js
+++ src/Component/Select/index.js
@@ -1,19 +1,17 @@
+import styled from "styled-components";
 
-import "./style.css";
-
+const StyledSelect = styled.select`
+  padding: 5px;
+  margin: 10px auto;
+  width: 100px;
+  cursor: pointer;
+`;
 export const Select = ({ currencies, setSelectedCurrencyId }) => (
-  <select
-    className="select"
-    onChange={({ target }) => setSelectedCurrencyId(target.value)}
-
-  >
-    {currencies.map(currency => (
-      <option
-        key={currency.id}
-        value={currency.id}
-      >
+  <StyledSelect onChange={({ target }) => setSelectedCurrencyId(target.value)}>
+    {currencies.map((currency) => (
+      <option key={currency.id} value={currency.id}>
         {currency.name}
       </option>
     ))}
-  </select >
+  </StyledSelect>
 );
