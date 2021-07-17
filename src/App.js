655af482e6a7eace43d6d6425e59c0c3c22275d4
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-07-17 19:05:22 +0200

    RadioChecked renamed to checkBox and added themes colors

--- src/App.js
+++ src/App.js
@@ -1,100 +1,89 @@
-
 import { useState, useEffect } from "react";
 import { Main } from "./Component/Main";
 import { Logo } from "./Component/Logo";
 import { Form } from "./Component/Form";
 import { Fieldset } from "./Component/Fieldset";
 import { Select } from "./Component/Select";
-import { RadioCheck } from "./Component/RadioCheck";
+import { Checkbox } from "./Component/Checkbox";
 import { Result } from "./Component/Result";
 import { Button } from "./Component/Button";
 import { ImputNumber } from "./Component/ImputNumber";
 import { Clock } from "./Component/Clock";
 import { currencies } from "./currencies/currencies";
-import  { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 
 const theme = {
-  colors:{
-    mainColor: "red",
+  colors: {
+    mainColor: "rgb(82, 231, 211)",
     fontColor: "rgb(250, 250, 250)",
+    checkedColor: "rgb(3, 238, 3)",
+    unCheckedColor: "rgb(255, 3, 3)",
   },
 };
 
 function App() {
-
-  const [result, setResult] = useState(() => (0));
-  const [resultUpdate, setResultUpdate] = useState(() => (0));
+  const [result, setResult] = useState(() => 0);
+  const [resultUpdate, setResultUpdate] = useState(() => 0);
   const [date, setDate] = useState(() => new Date());
 
-
-  const [amount, setAmont] = useState(() => (0));
-  const [selectedCurrencyId, setSelectedCurrencyId] = useState(() => (0));
-  const [targetCurrencyId, setTargetCurrencyId] = useState(() => (1));
+  const [amount, setAmont] = useState(() => 0);
+  const [selectedCurrencyId, setSelectedCurrencyId] = useState(() => 0);
+  const [targetCurrencyId, setTargetCurrencyId] = useState(() => 1);
 
   const calculateResult = (amount, selectedCurrencyId, targetCurrencyId) => {
     amount = +amount;
-    let result = selectedCurrencyId * amount / targetCurrencyId;
+    let result = (selectedCurrencyId * amount) / targetCurrencyId;
     result = result.toFixed(2);
     setResult(result);
-  }
+  };
 
   useEffect(() => {
-    let intervalID = setInterval(() =>
-      setDate(new Date())
-      , 1000);
+    let intervalID = setInterval(() => setDate(new Date()), 1000);
 
     return () => clearInterval(intervalID);
   }, []);
 
   return (
     <ThemeProvider theme={theme}>
-    <Main>
-      <Logo />
-      <Clock
-        date={date}
-      />
-      <Form
-        amount={amount}
-        selectedCurrencyId={selectedCurrencyId}
-        targetCurrencyId={targetCurrencyId}
-        setResultUpdate={setResultUpdate}
-        calculateResult={calculateResult}
-        result={result}
-      >
-        <Fieldset
-          title="Kwota do przeliczenia"
-          extraContent={
-            <ImputNumber
-              setAmont={setAmont}
-            />}
-          body={
-            <Select
-              currencies={currencies}
-              setSelectedCurrencyId={setSelectedCurrencyId}
-            />}
-        />
-        <Fieldset
-          title="Na jaką walutę"
-          body={
-            <RadioCheck
-              currencies={currencies}
-              setTargetCurrencyId={setTargetCurrencyId}
-              selectedCurrencyId={selectedCurrencyId}
-              targetCurrencyId={targetCurrencyId}
-            />}
-        />
-        <Fieldset
-          title="Wynik"
-          extraContent={
-            <Result
-              resultUpdate={resultUpdate}
-            />}
-        />
-        <Button
-          title="przelicz"
-        />
-      </Form>
-    </Main>
+      <Main>
+        <Logo />
+        <Clock date={date} />
+        <Form
+          amount={amount}
+          selectedCurrencyId={selectedCurrencyId}
+          targetCurrencyId={targetCurrencyId}
+          setResultUpdate={setResultUpdate}
+          calculateResult={calculateResult}
+          result={result}
+        >
+          <Fieldset
+            title="Kwota do przeliczenia"
+            extraContent={<ImputNumber setAmont={setAmont} />}
+            body={
+              <Select
+                currencies={currencies}
+                setSelectedCurrencyId={setSelectedCurrencyId}
+              />
+            }
+          />
+          <Fieldset
+            title="Na jaką walutę"
+            body={
+              <Checkbox
+                currencies={currencies}
+                setTargetCurrencyId={setTargetCurrencyId}
+                selectedCurrencyId={selectedCurrencyId}
+                targetCurrencyId={targetCurrencyId}
+              />
+            }
+          />
+          <Fieldset
+            title="Wynik"
+            extraContent={<Result resultUpdate={resultUpdate} />}
+          />
+          <Button title="przelicz" />
+        </Form>
+      </Main>
     </ThemeProvider>
   );
 }
