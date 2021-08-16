aa9bda83d98d4ed8b4772edb1e0f7005ac8493da
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-08-16 09:53:28 +0200

    currencies stylling moved to outer file

--- src/currencies/currencies.js
+++ src/currencies/currencies.js
@@ -3,15 +3,7 @@ import flagaPolski from "../images/flagaPolski.jpg";
 import flagaSzwajcarii from "../images/flagaSzwajcarii.jpg";
 import flagaUSA from "../images/flagaUSA.jpg";
 import flagaWielkiejBrytani from "../images/flagaWielkiejBrytani.jpg";
-import styled from "styled-components";
-
-const StyledFlagImage = styled.img`
-  position: absolute;
-  width: 25px;
-  height: 20px;
-  border-radius: 20%;
-  left: 15px;
-`;
+import { FlagImage } from "./styled";
 
 export const currencies = [
   {
@@ -19,36 +11,34 @@ export const currencies = [
     name: "PLN",
     fullName: "Złotych",
     rate: 1,
-    flag: <StyledFlagImage src={flagaPolski} alt="flaga Polski" />,
+    flag: <FlagImage src={flagaPolski} alt="flaga Polski" />,
   },
   {
     id: 1,
     name: "CHF",
     fullName: "Franków Szwajcarskich",
     rate: 4.0901,
-    flag: <StyledFlagImage src={flagaSzwajcarii} alt="flaga Szwajcarii" />,
+    flag: <FlagImage src={flagaSzwajcarii} alt="flaga Szwajcarii" />,
   },
   {
     id: 2,
     name: "USD",
     fullName: "Dolarów Amerykańskich",
     rate: 3.669,
-    flag: <StyledFlagImage src={flagaUSA} alt="flaga Szwajcarii" />,
+    flag: <FlagImage src={flagaUSA} alt="flaga Szwajcarii" />,
   },
   {
     id: 3,
     name: "GBP",
     fullName: "Funtów brytyjskich",
     rate: 5.1889,
-    flag: (
-      <StyledFlagImage src={flagaWielkiejBrytani} alt="flaga Wielkiej ytanii" />
-    ),
+    flag: <FlagImage src={flagaWielkiejBrytani} alt="flaga Wielkiej ytanii" />,
   },
   {
     id: 4,
     name: "EUR",
     fullName: "Euro",
     rate: 4.4674,
-    flag: <StyledFlagImage src={flagaEuro} alt="flaga Uni Europejskiej" />,
+    flag: <FlagImage src={flagaEuro} alt="flaga Uni Europejskiej" />,
   },
 ];
---
+++ src/currencies/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FlagImage = styled.img`
+  position: absolute;
+  width: 25px;
+  height: 20px;
+  border-radius: 20%;
+  left: 15px;
+`;
